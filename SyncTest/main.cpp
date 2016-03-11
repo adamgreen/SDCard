f73a296f2aac6a7d54b21ecce9310845ecd9e70e
Adam Green <adamgr@foo.bar>
2016-03-11 15:57:31 -0800

    Rename FFlushTest/ to SyncTest/
    Make the folder/project name consistent with the new API name from
    previous commit.

--- SyncTest/main.cpp
+++ SyncTest/main.cpp
@@ -12,7 +12,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-/* Exercise my FATFileSystem::fflush() code. */
+/* Exercise my FATFileSystem::sync() code. */
 #include <mbed.h>
 #include <SDFileSystem.h>
 
